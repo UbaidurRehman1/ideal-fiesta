a87ae39b141b5d2a727fc2bd51629cdae60eaca1
UbaidurRehman1 <ubaidshan007@gmail.com>
2020-02-26 19:55:02 +0500

    added

--- oktaIntegration/rest-client/src/main/java/com/codemaster/okta/restclient/config/OktaConfig.java
+++ oktaIntegration/rest-client/src/main/java/com/codemaster/okta/restclient/config/OktaConfig.java
@@ -1,9 +1,5 @@
 package com.codemaster.okta.restclient.config;
 
-import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
-import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
-import org.springframework.security.oauth2.config.annotation.web.configuration.EnableResourceServer;
-
 // @EnableWebSecurity
 // @EnableResourceServer
 // @EnableGlobalMethodSecurity(prePostEnabled = true)
